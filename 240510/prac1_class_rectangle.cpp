3399b0bc648dd6cc436ca6fe0e33f929a91ba4e5
YejeongKang <dpwjd1130@gmail.com>
2024-05-10 22:50:16 +0900

    Add project files.

---
+++ 240510/prac1_class_rectangle.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Rectangle {
+
+private:
+	float m_width = 0;
+	float m_height = 0;
+
+public:
+	Rectangle() {}
+
+	Rectangle(float w, float h) : m_width(w), m_height(h) {}
+
+	float area() { return m_width * m_height; }
+
+};
+
+int main() {
+	float width, height;
+
+	cout << "사각형의 가로와 세로 길이를 입력하시오. (띄어쓰기로 구분): ";
+	cin >> width >> height;
+	
+	Rectangle R(width, height);
+
+	cout << "사각형의 넓이는: " << R.area() << endl;
+
+	return 0;
+}
